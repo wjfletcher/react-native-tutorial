7c0ac7189a15365f59b03cbfd8c25d1028f6ebbd
wjfletcher <warrenjfletcher@gmail.com>
2017-04-07 10:34:40 -0400

    initial commit

---
+++ index.android.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+class AwesomeProject extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
